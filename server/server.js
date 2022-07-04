cf871ea42f9a361de5ff643798f82a516a3444f3
Calvin Cao <jtcao430@gmail.com>
2022-07-04 13:50:00 -0700

    added cors

--- server/server.js
+++ server/server.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const nestedTree = require('./controllers/nestedTree');
 const app = express();
+const cors = require('cors');
 const port = process.env.PORT || 8000;
+
+app.use(cors());
 
 app.get('/', (req, res) => {
   res.status(200).send('welcome to my api');
@@ -9,6 +12,21 @@ app.get('/', (req, res) => {
 
 app.get('/nested', nestedTree.get, (req, res) => {
   res.status(200).send(res.locals.list);
+});
+
+app.use('/*', (req, res) => {
+  res.sendStatus(404);
+});
+
+app.use((err, req, res, next) => {
+  const defaultErr = {
+    log: 'Express error handler caught unknown middleware error',
+    status: 500,
+    message: { err: 'An error occurred' },
+  };
+  const errorObj = Object.assign({}, defaultErr, err);
+  console.log(errorObj.log);
+  return res.status(errorObj.status).json(errorObj.message);
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
